760dacbaea59c522eb808264b81fe0098c0e5be5
Poojavpatel <32796407+Poojavpatel@users.noreply.github.com>
2019-05-21 22:10:49 +0530

    trying to solve vulnerability issue

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,10 +5,16 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
+
   constructor(private http: HttpClient) {
 
     getUsers(){
       return this.http.get('https://jsonplaceholder.typicode.com/users')
     }
+    
+    getUsers(userId){
+      
+    }
+
   }
 }
